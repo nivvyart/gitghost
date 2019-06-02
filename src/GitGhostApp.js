b6361bfa1647fb9c27342f677c8d2a216f518cdc
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-06-02 19:01:35 +1000

    project tidy up, axios added and github requests

---
+++ src/GitGhostApp.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "@patternfly/react-core/dist/styles/base.css";
+import { Grid, GridItem, Button } from "@patternfly/react-core";
+
+function GitGhostApp() {
+  return (
+    <div className="Container">
+      <h1>hi</h1>
+      <Button variant="primary">Button</Button>
+    </div>
+  );
+}
+
+export default GitGhostApp;
