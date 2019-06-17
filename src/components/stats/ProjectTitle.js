a0d10deea0e5ceef7132207eb3701cd1c8f4b323
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-06-17 15:49:37 +1000

    readme updates

--- src/components/stats/ProjectTitle.js
+++ src/components/stats/ProjectTitle.js
@@ -12,7 +12,6 @@ const ProjectTitle = props => {
       <p className="h5">
         Date Range :
         <span className="text-muted">
-          {" "}
           {props.startDate} > {props.endDate}
         </span>
       </p>
