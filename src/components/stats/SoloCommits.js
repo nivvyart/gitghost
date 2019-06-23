fc336ba5fe7765b28277929dd26d4381ca77f857
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-06-23 12:56:29 +1000

    started tidy up of solo commits, data good & console logging

--- src/components/stats/SoloCommits.js
+++ src/components/stats/SoloCommits.js
@@ -15,64 +15,35 @@ class SoloCommits extends Component {
       endDate: props.endDate + "T23:59:59.999Z",
       username: props.username,
       repository: props.repository,
-      winner: "",
-      highScore: 0
+      winner: "me",
+      highScore: 0,
+      winnerURL: "http://hi.com"
     };
   }
 
-  componentDidMount() {
-    const username = this.state.username;
-    const repository = this.state.repository;
-
-    const requests = [];
-
-    Github.getUserRepo(username, repository).then(result => {
-      result.data.forEach(commit => {
-        requests.push(
-          axios
-            .get(
-              `https://api.github.com/repos/${username}/${repository}/commits/${
-                commit.sha
-              }`
-            )
-            .then(result => {
-              let data;
-              if (
-                result.data.commit.author.date > this.state.startDate &&
-                result.data.commit.author.date < this.state.endDate
-              ) {
-                data = {
-                  author: result.data.author.login,
-                  additions: result.data.stats.additions
-                };
-                this.setState({ results: [...this.state.results, data] });
-              }
-            })
-        );
-      });
-      Promise.all(requests).then(() => {
-        // TODO: call findHighest
-        this.findHigest();
-      });
-    });
-  }
-
-  findHigest() {
+  findHigest(data) {
     let testing = {};
+    console.log(data);
+    console.log(data.repository.defaultBranchRef.target.history.nodes.length);
 
-    this.state.results.forEach(el => {
-      if (!testing[el.author]) {
-        testing[el.author] = el.additions;
-      } else {
-        testing[el.author] += el.additions;
+    data.repository.defaultBranchRef.target.history.nodes.forEach(el => {
+      if (el.author.user != null) {
+        console.log(el.additions);
+        console.log(el.author.user.login);
       }
     });
-    let winner = Object.keys(testing).reduce((a, b) =>
-      testing[a] > testing[b] ? a : b
-    );
-    this.setState({ winner: winner });
-    this.setState({ highScore: testing[winner] });
-    this.setState({ winnerURL:})
+    // if (!testing[el.author]) {
+    //     testing[el.author] = el.additions;
+    //   } else {
+    //     testing[el.author] += el.additions;
+    //   }
+    // });
+    // let winner = Object.keys(testing).reduce((a, b) =>
+    //   testing[a] > testing[b] ? a : b
+    // );
+    // this.setState({ winner: winner });
+    // this.setState({ highScore: testing[winner] });
+    // this.setState({ winnerURL: "winner" });
   }
 
   render() {
@@ -91,10 +62,20 @@ class SoloCommits extends Component {
                       until: "${this.state.endDate}"
                     ) {
                       nodes {
-                        committedDate
                         additions
                         author {
-                          avatarUrl
+                          user {
+                            login
+                            url
+                            avatarUrl
+                            
+                            followers {
+                              totalCount
+                            }
+                            starredRepositories {
+                              totalCount
+                            }
+                          }
                         }
                       }
                     }
@@ -109,32 +90,13 @@ class SoloCommits extends Component {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error </p>;
 
+          this.findHigest(data);
+
           if (data)
             return (
               <div>
-                <div className="row mt-3 border">
-                  <div className="col-md-4 ">
-                    <img src={data.user.avatarUrl} />
-                  </div>
-                  <div className="col-md-8 card-body ">
-                    <h3 className="font-weight-bold">Speed Demon</h3>
-                    <a target="_blank" href={data.user.url}>
-                      <h5>{this.state.winner}</h5>
-                    </a>
-                    <ul className="list-inline">
-                      <li className="list-inline-item">
-                        Commits total: {this.state.highScore}
-                      </li>
-                      <li className="list-inline-item">
-                        Followers: {data.user.followers.totalCount}
-                      </li>
-                      <li className="list-inline-item">
-                        Starred Repositories:{" "}
-                        {data.user.starredRepositories.totalCount}
-                      </li>
-                    </ul>
-                  </div>
-                </div>
+                <h1>{this.state.winner}</h1>
+                <h2>{this.state.winnerURL}</h2>
               </div>
             );
         }}
