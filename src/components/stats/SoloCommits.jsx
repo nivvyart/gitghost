4485363cf957b658638d6c3bca3f045ea1cae1b6
Adrian Thorburn <adrian.thorburn@gmail.com>
2019-06-28 11:36:57 +1000

    axios removed and tested from commit total

--- src/components/stats/SoloCommits.jsx
+++ src/components/stats/SoloCommits.jsx
@@ -43,18 +43,17 @@ class SoloCommits extends Component {
             }
           }
         }
-      }`
+      }`,
     })
-      .then(result => console.log(result));
+      .then((result) => {
+        this.findHigest(result);
+      });
   }
 
   // eslint-disable-next-line class-methods-use-this
   findHigest(data) {
     const testing = {};
-    console.log(data);
-    console.log(data.repository.defaultBranchRef.target.history.nodes.length);
-
-    const formatted = data.repository.defaultBranchRef.target.history.nodes;
+    const formatted = data.data.repository.defaultBranchRef.target.history.nodes;
 
     formatted.forEach((el) => {
       if (el.author.user != null) {
@@ -64,25 +63,11 @@ class SoloCommits extends Component {
           // if testing user does exist - add addition to previous user
           testing[el.author.user.login] += el.additions;
         }
-        // returns  - { key(username) : object(commit total) }
-        console.log(testing);
       }
-
-      // this.setState({ winnerURL: "winner" });
     });
-    const winner = Object.keys(testing).reduce((a, b) => (testing[a] > testing[b] ? a : b));
-
-    // returns winner and high score
-    console.log('winner, should only happen once', winner, testing[winner]);
-
-    // setting state here crashes react, you cant set the state
-    // multiple times from within the render
-    // this.setState({ winner: winner });
-    // this.setState({ highScore: testing[winner] });
-
-    // i need an object here that has more that winner
-    // and commit total so i can use that to build the
-    // card, rather than recalling GQL like in the axios example
+    const highScoreWinner = Object.keys(testing).reduce((a, b) => (testing[a] > testing[b] ? a : b));
+    this.setState({ winner: highScoreWinner });
+    this.setState({ highScore: testing[highScoreWinner] });
   }
 
   render() {
@@ -90,30 +75,15 @@ class SoloCommits extends Component {
       <Query
         query={gql`
           {
-            repository(owner: "${this.state.username}", name: "${this.state.repository}") {
-              defaultBranchRef {
-                target {
-                  ... on Commit {
-                    history(since: "${this.state.startDate}" until: "${this.state.endDate}") {
-                      nodes {
-                        additions
-                        author {
-                          user {
-                            login
-                            url
-                            avatarUrl
-                            followers {
-                              totalCount
-                            }
-                            starredRepositories {
-                              totalCount
-                            }
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
+            user(login: "${this.state.winner}") {
+              avatarUrl
+              url
+
+              followers {
+                totalCount
+              }
+              starredRepositories {
+                totalCount
               }
             }
           }
@@ -123,17 +93,37 @@ class SoloCommits extends Component {
           if (loading) return <p>Loading...</p>;
           if (error) return <p>Error </p>;
 
-          this.findHigest(data);
-
           if (data) {
             return (
               <div>
-                <h1>{this.state.winner}</h1>
-                <h2>{this.state.highScore}</h2>
+                <div className="row mt-3 border">
+                  <div className="col-md-4 ">
+                    <img src={data.user.avatarUrl} alt={this.state.winner} />
+                  </div>
+                  <div className="col-md-8 card-body ">
+                    <h3 className="font-weight-bold">Refactor King</h3>
+                    <a target="_blank" href={data.user.url}>
+                      <h5>{this.state.winner}</h5>
+                    </a>
+                    <ul className="list-inline">
+                      <li className="list-inline-item">
+                        Refactor total: {this.state.highScore}
+                      </li>
+                      <li className="list-inline-item">
+                        Followers: {data.user.followers.totalCount}
+                      </li>
+                      <li className="list-inline-item">
+                        Starred Repositories:{" "}
+                        {data.user.starredRepositories.totalCount}
+                      </li>
+                    </ul>
+                  </div>
+                </div>
               </div>
             );
           }
-        }}
+        }
+        }
       </Query>
     );
   }
